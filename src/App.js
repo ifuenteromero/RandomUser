2b1b2c560da1bf83cf004d06f772c3c95a513e60
Irene Fuente <ifuenteromero@gmail.com>
2019-05-18 11:29:36 +0200

    Fetch and save in the state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: [],
+    }
+    this.getData();
+  }
+
+  getData() {
+    fetch('https://randomuser.me/api/?results=50')
+      .then(result => result.json())
+      .then(data => {
+        this.setState({
+          people: data.results,
+        })
+      })
+  }
+
+  render() {
+    const people = this.state.people;
+    console.log(people);
+    return (
+      <p>holi</p>
+    );
+  }
+
+
 }
+
 
 export default App;
