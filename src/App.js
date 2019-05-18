c7567a39857f600d2978718e85d1a0fcfb77e2f4
Irene Fuente <ifuenteromero@gmail.com>
2019-05-18 12:52:48 +0200

    add style to cards

--- src/App.js
+++ src/App.js
@@ -26,14 +26,23 @@ class App extends React.Component {
     return (
       <ul>
        {people.map((person,index)=>{
+         const imgSrc = person.picture.large;
          const fullName = `${person.name.first} ${person.name.last}`;
          const age = person.dob.age;
          const city = person.location.city;
          return(
-                 <li key={index} >
-                     <img src={person.picture.medium} alt={fullName} />
-                     <h1>{`${fullName}, ${age}`}</h1>
-                     <h2>{city}</h2>
+                 <li className="card mb-3" style={{maxWidth:540}} key={index} >
+                    <div className="row no-gutters">
+                        <div className="col-md-4">
+                            <img className="card-img"  src={imgSrc} alt={fullName} />
+                        </div>
+                        <div className="col-md-8">
+                              <div className="card-body">
+                                  <h5 className="card-title">{`${fullName}, ${age}`}</h5>
+                                  <p className="card-text">{city}</p>
+                              </div>
+                          </div>
+                      </div>
                  </li>
           )})}
       </ul>
