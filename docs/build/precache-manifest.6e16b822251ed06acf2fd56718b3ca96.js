bd297f55a719def5301091aadad842fb5c114b11
Irene Fuente <ifuenteromero@gmail.com>
2019-05-18 12:24:05 +0200

    update docs folder

---
+++ docs/build/precache-manifest.6e16b822251ed06acf2fd56718b3ca96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a1e04f7eb73f369261289fee518590",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33921ab103949461c07d",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "./static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "33921ab103949461c07d",
+    "url": "./static/js/main.cc5534b2.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
